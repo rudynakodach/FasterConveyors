2b37c1d3e6a57626b48aa3b3ffa79f069ca9f75d
rudynakodach <whoppe7292@gmail.com>
2023-04-10 12:50:53 +0200

    fixed "oh no" and added some additional info for the thorium conveyor

--- src/com/github/rudynakodach/content/FCBlocks.java
+++ src/com/github/rudynakodach/content/FCBlocks.java
@@ -1,6 +1,7 @@
 package com.github.rudynakodach.content;
 
 import mindustry.content.Items;
+import mindustry.content.TechTree;
 import mindustry.core.ContentLoader;
 import mindustry.type.Category;
 import mindustry.type.ItemStack;
@@ -17,6 +18,7 @@ public class FCBlocks extends ContentLoader {
         thoriumConveyor = new Conveyor("thorium-conveyor"){{
            requirements(Category.distribution, with(Items.graphite, 2, Items.silicon, 2, Items.thorium, 3));
            speed = .1f;
+           displayedSpeed = 14;
            health = 120;
            canOverdrive = true;
            size = 1;
